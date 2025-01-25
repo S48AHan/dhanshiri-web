537022b74c8305c72c40145df4eb1782dcffc04e
S48AHan <saberahmedn2@gmail.com>
2025-01-26 02:25:57 +0600

    navabar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,11 @@
+import { heroui } from "@heroui/react";
 import type { Config } from "tailwindcss";
-import { heroui } from "@heroui/theme";
-
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
- 
   ],
   theme: {
     extend: {
