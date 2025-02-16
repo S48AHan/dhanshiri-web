c139fb58bd60403897e7e24398aa24cc38528c35
S48AHan <saberahmedn2@gmail.com>
2025-02-17 01:37:56 +0600

    footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -8,15 +8,7 @@ export default function Footer() {
           <h2 className="text-4xl md:text-5xl font-bold tracking-wider">FIRST RUBBER</h2>
 
           <p className="text-sm md:text-base leading-relaxed opacity-90">
-            Hebei First Rubber Medical Technology Co., Ltd, as one of the main subsidiaries of Desheng Investment Group,
-            is established in 1965 with a 150,000 square meters factory area which is located in Zhengding Technology
-            Industrial Park, Hebei, China. Accumulated more than 57 years' experience and with a total investment of
-            CNY550 million, First Rubber is the researcher and manufacturer of the first Butyl rubber stopper in China.
-            With an annual output of 10 billion pcs rubber stoppers, First Rubber is one of the leading manufacturers
-            with standardization and specialization in Asia, with certificates U.S. DMF FDA, GMP(ISO 15378), ISO,
-            etc.The company's products include:bromobutyl rubber stopper,Eurocap for infusion,Wholesale lyo stopper
-            price,serum separator tubes supplier,slurry pump impeller,Cheapest rubber bulb for dropper,Best butyl rubber
-            for infusion, etc.
+            Content de............................................ footer e boshbe
           </p>
 
           <Link
