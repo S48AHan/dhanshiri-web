d9720bd7168cad285d53261b9225db09662dd15a
S48AHan <saberahmedn2@gmail.com>
2025-03-30 19:05:16 +0600

    About us

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './components/Hero'
+import Summary from './components/Summary'
 
 const page = () => {
   return (
     <div>
       <Hero/>
+      <Summary/>
     </div>
   )
 }
---
+++ src/components/Card/MasterCard.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+interface MasterCardProps {
+  title?: string;
+  description?: string;
+  content?: string;
+  footer?: string;
+}
+
+const MasterCard = ({
+  title,
+  description,
+  content,
+  footer,
+}: MasterCardProps) => {
+  return (
+    <Card className="border-2 border-gray-200 shadow-md p-4v mx-3 md:mx-10 xl:mx-[300px] my-3 md:my-5 xl:my-10">
+      <CardHeader>
+        {title ? <CardTitle>{title}</CardTitle> : <></>}
+        {description ? (
+          <CardDescription>{description}</CardDescription>
+        ) : (
+          <></>
+        )}
+      </CardHeader>
+      <CardContent>
+        <p>
+          {content
+            ? content
+            : ""}
+        </p>
+      </CardContent>
+      <CardFooter>{footer? <p>Card Footer</p>:<></>}</CardFooter>
+    </Card>
+  );
+};
+
+export default MasterCard;
