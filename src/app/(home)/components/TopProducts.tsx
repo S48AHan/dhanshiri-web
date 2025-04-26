89b1d08a1d8d433cd46c013a054ac8e0f315d6c6
S48AHan <saberahmedn2@gmail.com>
2025-04-27 01:04:58 +0600

    top products

--- src/app/(home)/components/TopProducts.tsx
+++ src/app/(home)/components/TopProducts.tsx
@@ -5,16 +5,25 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/Card/Carrowsel";
+import Image from "next/image";
 import React from "react";
 
 const TopProducts = () => {
   return (
-    <section className="p-4v mx-3 md:mx-10 xl:mx-[300px] my-3 md:my-5 xl:my-10">
-      <h1>Top Products</h1>
+    <section className="p-4v mx-10 xl:mx-[320px] my-3 md:my-5 xl:my-10">
+      <h1 className="font-semibold leading-none tracking-tight text-2xl">Top Products</h1>
       {/* <Carrowsel></Carrowsel> */}
       <Carousel>
         <CarouselContent>
-          <CarouselItem>...</CarouselItem>
+          <CarouselItem className="relative">
+            <p className="absolute">Printed Label</p>
+            <Image
+              height={300}
+              width={400}
+              alt="Printed Label"
+              src={"/Hero/Banner.jpg"}
+            />
+          </CarouselItem>
           <CarouselItem>...</CarouselItem>
           <CarouselItem>...</CarouselItem>
         </CarouselContent>
